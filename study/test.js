db5eb8695293dc33f99fca4d9fc03237e20e6a81
Goathoon <ownest2@gmail.com>
2023-03-20 00:43:47 +0900

    gitignore 추가

--- study/test.js
+++ study/test.js
@@ -1,3 +1,20 @@
-str = "Please visit here here here";
-str2 = str.replace(/here/g,"there");
-console.log(str2);
+const axios = require('axios');
+var URL = "https://1c41e2f9-20ad-471a-9dbb-0629e45e12cd.mock.pstmn.io/productList";
+//Postman 의 Mock(가짜) 서버 사용하여 클라이언트에서 GET메소드로 데이터 요청함.
+
+
+async function getData2() {
+    return (await axios.get(URL)).data;
+}
+
+async function calculateSum() {
+    var data = await getData2();
+    var total = 0;
+    for (var item of data) {
+        total += item.price;
+    }
+    console.log(total);
+}
+
+calculateSum();
+
