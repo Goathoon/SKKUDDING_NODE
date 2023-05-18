bd0bb8d8f6cc709584e955ab6b7962447fd69dcc
Goathoon <ownest2@gmail.com>
2023-05-18 16:20:54 +0900

    시험용

--- week6_Nest/week6/src/restaurant/restaurant.controller.ts
+++ week6_Nest/week6/src/restaurant/restaurant.controller.ts
@@ -1,33 +1,42 @@
-import { Controller,Param,Body,Get,Post,Delete,Put, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Body,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Patch,
+} from '@nestjs/common';
 import { RestaurantDto } from './dto/restaurant.dto';
 import { RestaurantService } from './restaurant.service';
 
 @Controller('restaurant')
 export class RestaurantController {
-    constructor(private restaurantService: RestaurantService) {}; //DI
+  constructor(private restaurantService: RestaurantService) {} //DI
 
-    @Get()
-    getAll(): RestaurantDto[] {
-        return this.restaurantService.getRestaurants();
-    }
+  @Get('/restaurants')
+  getAll() {
+    return this.restaurantService.getAllRestaurants();
+  }
 
-    @Get("/:name")
-    getOne(@Param("name") name:string): RestaurantDto {
-        return this.restaurantService.getRestaurantsByName(name);
-    }
+  //   @Get('/:name')
+  //   getOne(@Param('name') name: string): RestaurantDto {
+  //     return this.restaurantService.getRestaurantsByName(name);
+  //   }
 
-    @Post()
-    create(@Body() restaurant: any) {
-        return this.restaurantService.createRestaurant(restaurant);
-    };
+  //   @Post()
+  //   create(@Body() restaurant: any) {
+  //     return this.restaurantService.createRestaurant(restaurant);
+  //   }
 
-    @Delete("/:name")
-    remove(@Param("name") name: string) {
-        return this.restaurantService.deleteRestaurantByName(name);
-    };
-    
-    @Patch("/:name")
-    update(@Param("name") name:string, @Body() restaurantData: any){
-        return this.restaurantService.updateRestaurantByName(name,restaurantData);
-    }
+  //   @Delete('/:name')
+  //   remove(@Param('name') name: string) {
+  //     return this.restaurantService.deleteRestaurantByName(name);
+  //   }
+
+  //   @Patch('/:name')
+  //   update(@Param('name') name: string, @Body() restaurantData: any) {
+  //     return this.restaurantService.updateRestaurantByName(name, restaurantData);
+  //   }
 }
