60fb3b4862effd6ec0295e1ab59ca9b1c380dea0
Goathoon <ownest2@gmail.com>
2023-05-18 12:24:48 +0900

    prisma ORM 활용하여 DB연결하기

--- week6_Nest/week6/src/app.module.ts
+++ week6_Nest/week6/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantController } from './restaurant/restaurant.controller';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [RestaurantModule],
+  imports: [RestaurantModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- week6_Nest/week6/src/restaurant/restaurant.module.ts
+++ week6_Nest/week6/src/restaurant/restaurant.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RestaurantController } from './restaurant.controller';
 import { RestaurantService } from './restaurant.service';
-
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { Prisma } from '@prisma/client';
 @Module({
   controllers: [RestaurantController],
-  providers: [RestaurantService]
+  providers: [RestaurantService],
+  imports: [PrismaModule],
 })
 export class RestaurantModule {}
